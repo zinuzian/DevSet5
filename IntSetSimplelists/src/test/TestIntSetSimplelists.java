efd8d1646a52f4045b142212f8b5b102c02399e9
zinuzian <zinuzian@naver.com>
2018-06-20 22:04:09 +0900

    Bug fix
    there was a bug in insert method which is found by test codes.
    insert test and report test is added.
    ArrayList to LinkedList (type changed)

--- IntSetSimplelists/src/test/TestIntSetSimplelists.java
+++ IntSetSimplelists/src/test/TestIntSetSimplelists.java
@@ -1,156 +1,157 @@
 package test;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
 import java.util.Random;
 
-import junit.framework.Test;
+import org.junit.jupiter.api.Test;
+
 import junit.framework.TestCase;
-import junit.framework.TestSuite;
 import set.IntSetSimplelists;
 
 public class TestIntSetSimplelists extends TestCase{
 
 	IntSetSimplelists set;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		junit.textui.TestRunner.run(suite());
-	}
-	public static Test suite(){
-		return new TestSuite(IntSetSimplelists.class);
+
+	public Integer[] testTimeElapsed(int maxval, int maxelem){
+		try {
+			String s1,s2,s3,s4,s5;
+			File file = new File("maxval" + maxval + "maxelem" + maxelem+".txt");
+			BufferedWriter bw = new BufferedWriter(new FileWriter(file));
+
+			System.out.print("testTimeElapsed "); 
+			System.out.println("max value = " + maxval + " max element = " + maxelem);
+
+			
+			
+			
+			//Init
+			double total = 0.0;
+			double start = System.currentTimeMillis();
+
+			set = new IntSetSimplelists();
+			set.intSetImp(maxelem, maxval);
+			Random random = new Random();
+
+			double end = System.currentTimeMillis();
+			total += end-start;
+			System.out.println(s1="Initialize time elapsed : "+(end-start)/1000.0 +"sec");
+			
+			
+			//Insert
+			start = System.currentTimeMillis();
+			
+			while(set.size() < maxelem){
+				set.insert(random.nextInt(maxval));
+				if(set.size()%10000 == 0) {
+					System.out.println(set.size() + "elements inserted");
+				}
+			}
+			end = System.currentTimeMillis();
+			total += end-start;
+			System.out.println(s2="Insert time elapsed : "+(end-start)/1000.0 +"sec");
+
+			assertEquals(maxelem,set.size());
+
+
+			//Report
+			start = System.currentTimeMillis();
+
+			Integer[] report = set.report();
+
+			end = System.currentTimeMillis();
+			total += end-start;
+
+			System.out.println(s3="Report time elapsed : "+(end-start)/1000.0 +"sec");
+
+			System.out.println(s4="Total time elapsed : "+total/1000.0 +"sec");
+			System.out.println(s5="Space Complexity : "+set.size()*4+"\n\n");
+
+			// output file generation
+			bw.write(s1+"\n");
+			bw.write(s2+"\n");
+			bw.write(s3+"\n");
+			bw.write(s4+"\n");
+			bw.write(s5+"\n");
+			
+			String s = "{ ";
+			for(int i=0;i<maxelem;i++){
+				s += String.valueOf(report[i])+" ";
+				if(i%100==99){
+					s+="\n";
+					bw.write(s);
+					s="";
+				}
+			}
+			s += " }";
+			bw.write(s);
+
+			bw.close();
+			return report;
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
-	@Before
-	public void setUp(){
-		set = new IntSetSimplelists();
-	}
-
+//	@Test
+//	public void test1000000() {
+//		int n=1000000;
+//		testTimeElapsed(n,n/100);
+//		testTimeElapsed(n,n/50);
+//		testTimeElapsed(n,n/25);
+//
+//		
+//	}
+//	@Test
+//	public void test100000000() {
+//		int n=100000000;
+//		testTimeElapsed(n,n/100);
+//		testTimeElapsed(n,n/50);
+//		testTimeElapsed(n,n/25);
+//	}
 	@Test
-	public void testsimpleListInstantiateTest(){
-		// max elem = n/100, n/50, n/25
-		// max value = 100000000, 1000000
-
-		int n = 1000000;
-		set = new IntSetSimplelists();
-		set.intSetImp(n/100,n);
-		assertEquals(n/100,set.size());	
-
-		set = new IntSetSimplelists();
-		set.intSetImp(n/50,n);
-		assertEquals(n/50,set.size());	
-
-		set = new IntSetSimplelists();
-		set.intSetImp(n/25,n);
-		assertEquals(n/25,set.size());
-
-
-		n = 100000000;
-		set = new IntSetSimplelists();
-		set.intSetImp(n/100,n);
-		assertEquals(n/100,set.size());	
-
-		set = new IntSetSimplelists();
-		set.intSetImp(n/50,n);
-		assertEquals(n/50,set.size());
-
-		set = new IntSetSimplelists();
-		set.intSetImp(n/25,n);
-		assertEquals(n/25,set.size());
+	public void testReport() {
+		int n=1000000;
+		Integer[] report;
+		report=testTimeElapsed(n,n/100);
+		for(int i=0;i<report.length-1;i++) {
+			assertTrue(report[i] < report[i+1]);
+		}
 	}
-
 	@Test
-	public void testTimeElapsed1000000by100(){
-		
-		double total = 0.0;
-		double start = System.currentTimeMillis();
-		
+	public void testInsert() {
 		set = new IntSetSimplelists();
-		int maxval = 1000000;
-		int maxelem = maxval/100;
-		set.intSetImp(maxelem, maxval);
-		Random random = new Random();
+		set.intSetImp(10, 100);
+		set.insert(15);
+		set.insert(64);
+		set.insert(12);
+		set.insert(66);
+		set.insert(89);
+		set.insert(1);
+		set.insert(27);
+		assertEquals(set.size(), 7);
 		
-		double end = System.currentTimeMillis();
-		total += end-start;
-		System.out.println("Initialize time elapsed : "+(end-start)/1000.0 +"sec");
+		set.insert(35);
+		set.insert(35);
+		assertEquals(set.size(), 8);
 		
+		set.insert(100);	//boundary
+		set.insert(98232);
+		assertEquals(set.size(), 8);
 		
-		
-		
-		start = System.currentTimeMillis();
-		
-		do{
-			set.insert(random.nextInt(maxval));
-		}while(set.size() <= maxelem);
-
-		end = System.currentTimeMillis();
-		total += end-start;
-		System.out.println("Insert time elapsed : "+(end-start)/1000.0 +"sec");
-		
-		
-		
-		
-		start = System.currentTimeMillis();
+		set.insert(30);//9
+		set.insert(31);//10
+		set.insert(32);//10
+		set.insert(33);//10
+		assertEquals(set.size(), 10);
 		
 		Integer[] report = set.report();
-		
-		end = System.currentTimeMillis();
-		total += end-start;
-		System.out.println("Report time elapsed : "+(end-start)/1000.0 +"sec");
-		
-		System.out.println("Total time elapsed : "+total/1000.0 +"sec");
-		System.out.println("Space Complexity : "+set.size()*4);
-	}
-	/*
-	//@Test
-	public void testTimeElapsed1000000by50(){
-		set = new IntSetSimplelists();
-		int maxval = 1000000;
-		int maxelem = maxval/50;
-		set.intSetImp(maxelem, maxval);
-		Random random = new Random();
-
-		do{
-			set.insert(random.nextInt(maxval));
-		}while(set.size() <= maxelem);
-
-		Integer[] report = set.report();
-		for(int i=0;i<report.length-1;i++){
-			assertNotSame(report[i],report[i+1]);
+		for(int i=0;i<report.length-1;i++) {
+			assertTrue(report[i] < report[i+1]);
 		}
 	}
-	//@Test
-	public void testTimeElapsed1000000by25(){
-		set = new IntSetSimplelists();
-		int maxval = 1000000;
-		int maxelem = maxval/25;
-		set.intSetImp(maxelem, maxval);
-		Random random = new Random();
 
-		do{
-			set.insert(random.nextInt(maxval));
-		}while(set.size() <= maxelem);
-
-		Integer[] report = set.report();
-		for(int i=0;i<report.length-1;i++){
-			assertNotSame(report[i],report[i+1]);
-		}
-	}
-	//@Test
-	public void testTimeElapsed100000000by100(){
-		set = new IntSetSimplelists();
-		int maxval = 100000000;
-		int maxelem = maxval/100;
-		set.intSetImp(maxelem, maxval);
-		Random random = new Random();
-
-		do{
-			set.insert(random.nextInt(maxval));
-		}while(set.size() <= maxelem);
-
-		Integer[] report = set.report();
-		for(int i=0;i<report.length-1;i++){
-			assertNotSame(report[i],report[i+1]);
-		}
-	}
-	*/
 }
